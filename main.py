17981e76a65efd55d9f69f0534bd3514652c8041
NawafIT <nwef918@gmail.com>
2023-10-06 20:43:11 +0300

    New Update, use ttkbootstrap to make UI modern

--- main.py
+++ main.py
@@ -1,12 +1,20 @@
-import tkinter
+import ttkbootstrap as tkk
 from tkinter import *
 from PIL import Image, ImageTk
 from pytube import YouTube
 import tkinter.messagebox
 
-root = Tk()
+root = tkk.Window()
 root.title("DownloadVid")
-root.geometry("1000x400+500+200")
+screen_width = root.winfo_screenwidth()
+screen_height = root.winfo_screenheight()
+window_width = 1000
+window_height = 400
+x_position = (screen_width - window_width) // 2
+y_position = (screen_height - window_height) // 2
+
+root.geometry(f"{window_width}x{window_height}+{x_position}+{y_position - 60}")
+
 root.resizable(False, False)
 root.iconbitmap("C:/Users/nawaf/Downloads/Ava.ico")
 
@@ -49,7 +57,7 @@ def downloadAudio(url: str):
             tkinter.messagebox.showinfo("Audio", "Download completed")
 
 
-frame = Frame(root, width=1219, height=585, bg="#394867")
+frame = tkk.Frame(root, width=1219, height=585, style="dark")
 frame.place(x=0, y=0)
 image = Image.open("C:/Users/nawaf/Downloads/nawaf.png")
 # Resize the image using resize() method
@@ -58,32 +66,31 @@ img = ImageTk.PhotoImage(resize_image)
 image = Label(frame, image=img)
 image.place(x=5, y=15)
 
-link = Label(frame,
-             text="Use this Python script to download a video \nfrom YouTube with the highest quality available.\nSimply paste the video link, and the script will handle the rest!",
-             font=("Overpass Mono", 13, "bold"), fg="white", bg="#212A3E")
+
+link = tkk.Label(frame,
+                 text="\t     Use this Python script to download a video \n\t   from YouTube with the highest quality available.\n  Simply paste the video link, and the script will handle the rest!",
+                 font=("Overpass Mono", 12, "bold"), bootstyle="inverse")
 link.place(x=280, y=15)
 
-text = Label(frame, text="Put Your Link here:",
-             font=("Overpass Mono", 13, "bold"), fg="white", bg="#212A3E")
+text = tkk.Label(frame, text="Put Your Link here:",
+                 font=("Overpass Mono", 13, "bold"), bootstyle="inverse-primary")
 text.place(x=520, y=150)
 
-enterLink = Entry(frame, bg="#B0DAFF", fg="#3E497A", font=("Overpass Mono", 13, "bold"), justify=CENTER,
-                  highlightbackground="#3E497A",
-                  highlightcolor="#D4ADFC", highlightthickness=2, width=60)
+enterLink = tkk.Entry(frame, font=("Overpass Mono", 13, "bold"), justify=CENTER,
+                      width=60, bootstyle="info")
 enterLink.place(x=300, y=200)
 
-button = Button(frame, width=50, bg="#B0DAFF", fg="#3E497A", text="Download Video", font=("Overpass Mono", 10, "bold"),
-                activebackground="#5C469C", activeforeground="white"
-                , command=lambda: downloadVid(enterLink.get()))
-button.place(x=400, y=270)
+button = Button(frame, width=50, text="Download Video", font=("Overpass Mono", 11, "bold"),
+                command=lambda: downloadVid(enterLink.get()))
 
-button2 = Button(frame, width=50, bg="#B0DAFF", fg="#3E497A", text="Download Audio", font=("Overpass Mono", 10, "bold"),
-                 activebackground="#5C469C", activeforeground="white"
-                 , command=lambda: downloadAudio(enterLink.get()))
-button2.place(x=400, y=320)
+button.place(x=385, y=270)
 
-text = Label(frame, text="Nawaf Al-Jehani",
-             font=("Overpass Mono", 13, "bold"), fg="white", bg="#212A3E")
-text.place(x=50, y=300)
+button2 = Button(frame, width=50, text="Download Audio", font=("Overpass Mono", 11, "bold"),
+                 command=lambda: downloadAudio(enterLink.get()))
+button2.place(x=385, y=320)
+
+text2 = tkk.Label(frame, text="Nawaf Al-Jehani",
+                  font=("Overpass Mono", 13, "bold"), bootstyle="inverse-danger")
+text2.place(x=50, y=300)
 
 root.mainloop()
